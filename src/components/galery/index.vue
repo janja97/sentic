57922f0c808e8955d1fb9fcd4fd0103c9f43e6c1
Janja Badrov <senticjanja@gmail.com>
2023-11-25 09:26:21 +0100

    fix seo

--- src/components/galery/index.vue
+++ src/components/galery/index.vue
@@ -24,7 +24,7 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title" :id="'exampleModalLabel' + index">{{ apartman.ime }}</h1>
+                <h5 class="modal-title" :id="'exampleModalLabel' + index">{{ apartman.ime }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body p-0">
